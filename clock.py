54a8a0a92bb0456a47a65bd753244c10c2dfbda5
Efty <efty131331@gmail.com>
2023-01-29 10:44:07 +0600

    date added

--- clock.py
+++ clock.py
@@ -1,31 +1,3 @@
-# import time
-# from datetime import datetime
-# from pytz import timezone
-# from tkinter import *
-
-# root = Tk()
-# root.geometry("359x150+0+0")
-# root.configure(background="black")
-# root.resizable(0,0)
-# # root.overrideredirect(1)  [this line is to hide the title bar ]
-
-# # Set the timezone to Bangladesh
-# bd_tz = timezone("Asia/Dhaka")
-
-# # Function that updates the time label
-# def start():
-#     text = datetime.now(bd_tz).strftime("%H:%M:%S")
-#     label.config(text = text)
-#     label.after(200,start)
-
-# # Create a time label
-# label = Label(root,font=("ds-digital",50,'bold'),bg="black",fg="aqua",bd=50)
-# label.grid(row=0,column=1)
-
-# # Start the update loop
-# start()
-# print("Clock is running")
-# root.mainloop()
 import time
 from datetime import datetime
 from pytz import timezone
@@ -33,18 +5,23 @@ from tkinter import *
 
 root = Tk()
 root.geometry("359x150+0+0")
-root.configure(background="black")
-# root.resizable(0,0)
+root.configure(background="black") 
 # root.overrideredirect(1)
 
 bd_tz = timezone("Asia/Dhaka")
 
 def start():
-    text = datetime.now(bd_tz).strftime("%I:%M:%S %p")
-    label.config(text = text)
-    label.after(200,start)
+    current_date = datetime.now(bd_tz).strftime("%B %d, %Y")
+    current_time = datetime.now(bd_tz).strftime("%I:%M:%S %p")
+    label_date.config(text = current_date)
+    label_time.config(text = current_time)
+    label_time.after(200,start)
 
-label = Label(root,font=("ds-digital",50,'bold'),bg="black",fg="aqua",bd=50)
-label.grid(row=0,column=1)
+label_time = Label(root, font=("ds-digital",50,'bold'),bg="black",fg="aqua",bd=50)
+label_time.grid(row=1,column=1)
+label_date = Label(root, font=("ds-digital",20,'bold'),bg="black",fg="blue",bd=50)
+label_date.grid(row=0,column=1)
+
 start()
+print("Done")
 root.mainloop()
